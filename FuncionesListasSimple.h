1c29b2b33233a994154418c2dc279906507925d9
miller-cano <73566526+miller-cano@users.noreply.github.com>
2020-11-06 00:35:56 -0500

    Update FuncionesListasSimple.h

--- FuncionesListasSimple.h
+++ FuncionesListasSimple.h
@@ -1,157 +1,161 @@
+#include <stdio.h>
 #include <iostream>
+#include <stdlib.h>
+#include <conio.h>
 
 using namespace std;
 
-struct Nodo{
-       int dato; 
-       struct Nodo *sgte;
-       struct Nodo *lista = NULL;
+/////////////////////////////////////////////
+struct Nodo
+{
+    int dato; 
+    struct Nodo *sgte;
 };
-
-struct Nodo *constList(Nodo *lista){
-	
-	int dato;
-	
-	cout<<"\nDigite el dato: ";
-	cin>>dato;
-	
-	struct Nodo *p,*nuevo;
-	nuevo = new Nodo;
+///////////////////////////////////////////////////////////
+void mostrarLista(struct Nodo *lista){
+	int i = 1;
+	struct Nodo *p;
+	p = lista;
+	cout<<"\n\t*****************************************";
+	cout<<"\n\t|                 LISTA                 |";
+	cout<<"\n\t*****************************************";
+	while(p != NULL) {
+		cout<<"\n\t|   "<<i<<") "<<p->dato;
+		p = p->sgte;
+		i++;
+	}
+	cout<<"\n\t*****************************************"<<endl;
+}
+///////////////////////////////////////////////////////////////////
+struct Nodo *crearLista(struct Nodo *lista, int dato)
+{	
+	struct Nodo *nuevo;
+    nuevo = new(struct Nodo);
 	nuevo->dato = dato;
-	
 	if(lista == NULL){
 		nuevo->sgte = NULL;
 		lista = nuevo;
-		cout<<"\nLista creada correctamente ";
+		cout<<"\n ***************************************************";
+		cout<<"\n |          LISTA CREADA Y DATO INSERTADO          |";
+		cout<<"\n ***************************************************"<<endl;
 	}
 	else{
 		nuevo->sgte = lista;
 		lista = nuevo;
-		cout<<"\nElemento ingresado correctamente ";
+		cout<<"\n ************************************";
+		cout<<"\n |          DATO INSERTADO          |";
+		cout<<"\n ************************************"<<endl;
 	}
-	
-	return (lista);
-	
+	return lista;
 }
-
-void contarIguales(Nodo *lista,int x, int y){
-	
+/////////////////////////////////////////////////
+void contarIguales(Nodo *lista,int x, int y)
+{
 	struct Nodo *p;
 	int i = 1;
-	int contadorx,contadory = 0;
+	int contX = 0, contY = 0;
 	bool band = false;
 	
 	if(lista == NULL){
-		cout<<"\nNo hay ninguna lista ";
+		cout<<"\n *****************************************";
+		cout<<"\n |          LA LISTA ESTA VACIA          |";
+		cout<<"\n *****************************************"<<endl;
 	}
 	else{
-	
 		p = lista;
-		
 		while(p != NULL){
-		
 			if(p->dato == x){
-				cout<<"\nElemento "<<x<<" encontrado en la posicion "<<i;
-				contadorx ++;
+				cout<<"\n ************************************************************************";
+				cout<<"\n |          ELEMENTO "<<x<<" ENCONTRADO EN LA POSICION ["<<i<<"]";
+				cout<<"\n ************************************************************************"<<endl;
+				contX++;
 				band = true;
 			}
-		
 			if(p->dato == y){
-				cout<<"\nElemento "<<y<<" encontrado en la posicion "<<i;
-				contadory ++;
+				cout<<"\n ************************************************************************";
+				cout<<"\n |          ELEMENTO "<<y<<" ENCONTRADO EN LA POSICION ["<<i<<"]";
+				cout<<"\n ************************************************************************"<<endl;
+				contY++;
 				band = true;
 			}
-		
 			p = p->sgte;
 			i++;
 		}	
-	
 		if(band == false){
-			cout<<"\nNo se ha encontrado el valor "<<x<<" ni el valor "<<y<<" en la lista ";
-		}else{
-			cout<<"\nEl elemento "<<x<<" se encontro "<<contadorx<<" veces en la lista ";
-			cout<<"\nEl elemento "<<y<<" se encontro "<<contadory<<" veces en la lista ";
+			cout<<"\n *************************************************************************************";
+			cout<<"\n |          NO SE ENCONTRO EL VALOR DE "<<x<<" NI EL VALOR "<<y<<" EN LA LISTA";
+			cout<<"\n *************************************************************************************"<<endl;
 		}
-	
-	
-	}
-	
+		else{
+			cout<<"\n *******************************************************************************";
+			cout<<"\n |          EL ELEMENTO "<<x<<" SE ENCONTRO "<<contX<<" VECES EN LA LISTA";
+			cout<<"\n *******************************************************************************"<<endl;
+			cout<<"\n *******************************************************************************";
+			cout<<"\n |          EL ELEMENTO "<<y<<" SE ENCONTRO "<<contX<<" VECES EN LA LISTA";
+			cout<<"\n *******************************************************************************"<<endl;
+		}
+	}	
 }
-
-struct Nodo interPos(Nodo *lista,int pos1, int pos2, int info1, int info2){
-	
+//////////////////////////////////////////////////////////////////////////
+struct Nodo *interPos(struct Nodo *lista, int pos1, int pos2)
+{
 	struct Nodo *p,*q,*r;
-	int i = 1;
-	int aux1 = 0;
-	bool band1,band2 = false;
-	
-	
+	int i = 1, aux1 = 0;
+	bool band1 = false ,band2 = false;
 	if(lista == NULL){
-		cout<<"\nNo hay ninguna lista ";
+		cout<<"\n *****************************************";
+		cout<<"\n |          LA LISTA ESTA VACIA          |";
+		cout<<"\n *****************************************"<<endl;
 	}
 	else{
-		
-		cout<<"\nDigite la posicion 1: ";
-		cin>>pos1;
-		
-		cout<<"\nDigite la posicion 2: ";
-		cin>>pos2;
-		
 		p = lista;
-		
 		while(p != NULL){
-			
 			if(pos1 == i){
 				q = p;
 				band1 = true;
 			}
-			
 			if(pos2 == i){
 				r = p;
 				band2 = true;
 			}
-			
-			p=p->sgte;
+			p = p->sgte;
 			i++;
 		}
-		
 		if((band1 == true) && (band2 == true)){
 			aux1 = q->dato;
 			q->dato = r->dato;
 			r->dato = aux1;
-			cout<<"\nLa informacion de los nodos se ha cambiado ";
+			cout<<"\n *********************************************************************";
+			cout<<"\n |          LA INFORMACION DE LOS NODOS SE HA INTERCAMBIADO          |";
+			cout<<"\n *********************************************************************"<<endl;
 		}
 		else{
-			cout<<"\nEl procedimiento no se pudo realizar, posicion no encontrada ";
-		}
-		
+			cout<<"\n ******************************************************";
+			cout<<"\n |          LAS POSICIONES NO SE ENCONTRARON          |";
+			cout<<"\n ******************************************************"<<endl;
+		}	
 	}
-
-
+	return lista;
 }
-
-struct Nodo *buscarInfor(Nodo *lista, int info1, int info2){
-	
+//////////////////////////////////////////////////////////////
+void buscarInfor(Nodo *lista, int info1, int info2)
+{
 	struct Nodo *p;
 	int i = 1;
-	
-		p = lista;
-		while(p!=NULL){
-			
-			if(p->dato == info1){
-				cout<<"\nLa informacion 1 ("<<info1<<") fue encontrada en la posicion "<<i;
-			}
-			
-			if(p->dato == info2){
-				cout<<"\nLa informacion 2 ("<<info2<<") fue encontrada en la posicion "<<i;
-			}
-			
-			
-			p=p->sgte;
-			i++;
+	p = lista;
+	while(p != NULL){	
+		if(p->dato == info1){
+			cout<<"\n *******************************************************************************";
+			cout<<"\n |          LA INFORMACION 1 ["<<info1<<"] SE ENCONTRO EN LA POSICION"<<i;
+			cout<<"\n *******************************************************************************"<<endl;
 		}
-	
-	
-	return (lista);
+		if(p->dato == info2){
+			cout<<"\n *******************************************************************************";
+			cout<<"\n |          LA INFORMACION 2 ["<<info2<<"] SE ENCONTRO EN LA POSICION"<<i;
+			cout<<"\n *******************************************************************************"<<endl;
+		}
+		p = p->sgte;
+		i++;
+	}
 }
-
+//////////////////////////////////////////////////////////////
